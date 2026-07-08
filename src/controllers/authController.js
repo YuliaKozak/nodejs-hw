f16becfd44e129df181f968c4aadd9357c777261
Yulia Kozak <ykozak74@gmail.com>
2026-07-08 19:05:34 +0300

    add way

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,7 +7,7 @@ import { Session } from "../models/session.js";
 import { createSession, setSessionCookies } from '../services/auth.js';
 
 import jwt from 'jsonwebtoken';
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 import handlebars from 'handlebars';
 import path from 'node:path';
